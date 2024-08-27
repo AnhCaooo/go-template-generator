7e28351aa5f1e531fb8b89adac12e30002d6fbf2
Anh Cao <anhcao4922@gmail.com>
2024-08-27 21:34:22 +0300

    add: start Gorilla/mux server
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- template/cmd/main.go
+++ template/cmd/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "github.com/AnhCaooo/go-web-server-template-generator/template/internal/logger"
+import (
+	"log"
+	"net/http"
+
+	"github.com/AnhCaooo/go-web-server-template-generator/template/internal/api/handlers"
+	"github.com/AnhCaooo/go-web-server-template-generator/template/internal/api/middleware"
+	"github.com/AnhCaooo/go-web-server-template-generator/template/internal/api/routes"
+	"github.com/AnhCaooo/go-web-server-template-generator/template/internal/logger"
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	// Initialize logger
 	logger.InitLogger()
+
+	// Initial new router
+	r := mux.NewRouter()
+	for _, endpoint := range routes.Endpoints {
+		r.HandleFunc(endpoint.Path, endpoint.Handler).Methods(endpoint.Method)
+	}
+	r.MethodNotAllowedHandler = http.HandlerFunc(handlers.NotAllowed)
+	r.NotFoundHandler = http.HandlerFunc(handlers.NotFound)
+
+	// Middleware
+	r.Use(middleware.Logger)
+
+	// Start server
+	logger.Logger.Info("Server started on :8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
--- template/go.mod
+++ template/go.mod
@@ -2,6 +2,9 @@ module github.com/AnhCaooo/go-web-server-template-generator/template
 
 go 1.21.6
 
-require go.uber.org/zap v1.27.0
+require (
+	github.com/gorilla/mux v1.8.1
+	go.uber.org/zap v1.27.0
+)
 
 require go.uber.org/multierr v1.11.0 // indirect
