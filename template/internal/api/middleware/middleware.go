8aa328e14f2b8659a0383a18bc5da64e7ef1ed78
Anh Cao <anhcao4922@gmail.com>
2024-08-27 21:28:20 +0300

    add: include templates for routes and middleware
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- template/internal/api/middleware/middleware.go
+++ template/internal/api/middleware/middleware.go
@@ -1,1 +1,15 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/AnhCaooo/go-web-server-template-generator/template/internal/logger"
+	"go.uber.org/zap"
+)
+
+func Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger.Logger.Info("request received", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
+		next.ServeHTTP(w, r)
+	})
+}
--- template/internal/api/routes/routes.go
+++ template/internal/api/routes/routes.go
@@ -1,1 +1,39 @@
 package routes
+
+import (
+	"net/http"
+)
+
+// Endpoint is the presentation of object which contains values for routing
+type Endpoint struct {
+	Path    string
+	Handler http.HandlerFunc
+	Method  string
+}
+
+var Endpoints = []Endpoint{
+	{
+		Path:    "/v1/get-request",
+		Handler: nil,
+		Method:  "GET",
+	},
+	{
+		Path:    "/v1/post-request",
+		Handler: nil,
+		Method:  "POST",
+	}, {
+		Path:    "/v1/patch-request",
+		Handler: nil,
+		Method:  "PATCH",
+	},
+	{
+		Path:    "/v1/put-request",
+		Handler: nil,
+		Method:  "PUT",
+	},
+	{
+		Path:    "/v1/delete-request",
+		Handler: nil,
+		Method:  "DELETE",
+	},
+}
