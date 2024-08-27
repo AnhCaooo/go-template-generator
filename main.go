9662011d73dd7b2b30aac9a5f750ca0519129338
Anh Cao <anhcao4922@gmail.com>
2024-08-27 22:17:00 +0300

    add: include todo comments for later implementation
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+// STEP 1: ask for which path the new repo will be located. If not given, then give default name 'go-web-server' and create it in current working directory.
+
+// STEP 2: navigate to recent created folder, navigate to.
+
+// STEP 3: do git init
+
+// STEP 4: ask for project path in order to intialize Go module. Good to provide some examples.
+
+// STEP 5: do copy all files from template and paste to current working directory
