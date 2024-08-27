40875e8d8351e799e77f2d1b44fc8e0f3538e417
Anh Cao <anhcao4922@gmail.com>
2024-08-27 21:22:06 +0300

    add: implement logger for template
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- template/cmd/main.go
+++ template/cmd/main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "github.com/AnhCaooo/go-web-server-template-generator/template/internal/logger"
 
+func main() {
+	// Initialize logger
+	logger.InitLogger()
 }
--- template/internal/logger/logger.go
+++ template/internal/logger/logger.go
@@ -1,1 +1,24 @@
 package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Logger *zap.Logger
+
+// initialize logger
+func InitLogger() {
+	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder // Use ISO8601 time format
+
+	// Disable JSON encoding
+	cfg.Encoding = "console"
+
+	l, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	Logger = l
+}
