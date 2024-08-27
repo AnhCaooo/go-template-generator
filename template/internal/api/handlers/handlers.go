9e578dd19374b219210cef1f89cabb67182cd2bf
Anh Cao <anhcao4922@gmail.com>
2024-08-27 21:29:52 +0300

    add: include handlers template
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- template/internal/api/handlers/handlers.go
+++ template/internal/api/handlers/handlers.go
@@ -1,1 +1,22 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/AnhCaooo/go-web-server-template-generator/template/internal/logger"
+	"go.uber.org/zap"
+)
+
+// return response when request url is not found
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	logger.Logger.Info("undefined endpoint", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
+	w.Write([]byte("404 - Not found"))
+}
+
+// return response when request method is not allowed
+func NotAllowed(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	logger.Logger.Info("method not allowed", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
+	w.Write([]byte("405 - Method not allowed"))
+}
